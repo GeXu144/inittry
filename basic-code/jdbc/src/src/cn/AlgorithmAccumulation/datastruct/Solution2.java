8d3d5aacd5f25219c1dfb525162edf35c1c2e9ab
GeXu144 <1446620486@qq.com>
2021-09-21 12:07:17 +0800

    算法+1

---
+++ basic-code/jdbc/src/src/cn/AlgorithmAccumulation/datastruct/Solution2.java
@@ -0,0 +1,16 @@
+package src.cn.AlgorithmAccumulation.datastruct;
+
+public class Solution2 {
+    public static void changeArray(int[] array){
+        int low=0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]!=0){
+                array[low]=array[i];
+                low+=1;
+            }
+        }
+        for (int i = low; i < array.length; i++) {
+            array[i]=0;
+        }
+    }
+}
