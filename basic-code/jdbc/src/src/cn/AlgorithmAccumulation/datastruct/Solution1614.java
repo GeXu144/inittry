420da85c3578936ecb8d5dacd815dad5be17a8f5
GeXu144 <1446620486@qq.com>
2022-01-07 13:58:20 +0800

    算法+1

---
+++ basic-code/jdbc/src/src/cn/AlgorithmAccumulation/datastruct/Solution1614.java
@@ -0,0 +1,28 @@
+package src.cn.AlgorithmAccumulation.datastruct;
+
+public class Solution1614 {
+
+    /**
+     * LeetCode-1614：寻找到最大的连续左括号数
+     *算法思想：1.遇到左括号+1，遇到右括号-1，统计最大值
+     * @param s
+     * @return
+     */
+    public static int maxDepth(String s) {
+        int n = s.length();
+        char[] item = s.toCharArray();
+        int ans=0;
+        for (int i = 0,cur=0; i < n; i++) {
+            //处理左括号
+            if (item[i]=='('){
+                cur++;
+                ans=Math.max(ans,cur);
+            }
+            //处理右括号
+            if (item[i]==')'){
+                cur--;
+            }
+        }
+        return ans;
+    }
+}
